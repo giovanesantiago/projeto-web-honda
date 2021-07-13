12511654eeb86f902143c7acff4cec9041e184c9
giovanesantiago <giovanedevweb@gmail.com>
2021-07-12 23:23:33 -0300

    Validaçao de qnt de letras em nome

--- Ladding-page-honda/cadastro/validacao.js
+++ Ladding-page-honda/cadastro/validacao.js
@@ -1,4 +1,22 @@
-function validar(){
-    var nomeV = document.getElementById('nome')
-   alert(nomeV.value)
+function validarNome() {
+    //variaveis
+    var nomeV = document.getElementById('nome') //Recebendo dado
+    var nome = String(nomeV.value) // adapitando leitura
+    qnt = Number(nome.length) // qnt de letras
+    var temNumero = temNumber(nome, qnt)
+    
+    //funções
+    function temNumber(nome, qnt) {
+        return true
+    }
+    
+    
+    //Validações
+    if (qnt <= 3) {
+        alert('[error] - Nome incompleto')
+    }else if (temNumero){
+        alert('ok')
+    }
+    
 }
+
